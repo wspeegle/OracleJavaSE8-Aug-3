e27bd67e32ca26fb2c098f897e738b68024350de
SimonHGR <simon@dancingcloudservices.com>
2015-08-04 12:10:46 -0400

    Added Scratch project, with static import example

---
+++ Scratch/src/scratch/Scratch.java
@@ -0,0 +1,12 @@
+package scratch;
+
+import static java.lang.Math.random;
+
+public class Scratch {
+
+    public static void main(String[] args) {
+        double d = random();
+        System.out.println("random is " + d);
+    }
+    
+}
